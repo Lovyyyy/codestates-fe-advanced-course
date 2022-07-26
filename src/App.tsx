e3f8bba649a85ddb07f2c1322fad85fa3d2e9821
Lovyyyy <cys7773@naver.com>
2022-07-26 18:46:38 +0900

    C.R.A

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
 
+interface postsDataInterface {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
 function App() {
+  const [postData, setPostData] = useState<postsDataInterface[]>();
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        console.log(res.data);
+        setPostData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(postData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HELLO MY LOVE!</h1>
     </div>
   );
 }
